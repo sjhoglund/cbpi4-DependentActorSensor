11dd5337f2443dfc7d6c83da4ce74e3540d0c9cc
Steven Hoglund <sjhoglund@gmail.com>
2022-02-01 15:06:51 -0800

    Try new function

--- cbpi4-DependentActorSensor/__init__.py
+++ cbpi4-DependentActorSensor/__init__.py
@@ -28,7 +28,7 @@ class DependentActorSensor(CBPiActor):
 
     async def on(self, power=0):
         sensor_dependency = self.cbpi.sensor.find_by_id(self.SensorDependency)
-        sensor_value = sensor_dependency.get("value")
+        sensor_value = self.cbpi.sensor.get_sensor_value(self.SensorDependency)
         
         if sensor_value >= self.SensorValue:
             await self.cbpi.actor.on(self.base)
