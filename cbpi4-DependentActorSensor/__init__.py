19307c98a19f82f077241d6a9d3a9b7a3484e35c
Steven Hoglund <sjhoglund@gmail.com>
2022-02-02 14:19:56 -0800

    Add dependent actor to meet my needs

--- cbpi4-DependentActorSensor/__init__.py
+++ cbpi4-DependentActorSensor/__init__.py
@@ -13,6 +13,7 @@ from cbpi.api.dataclasses import NotificationAction, NotificationType
 logger = logging.getLogger(__name__)
 
 @parameters([Property.Actor(label="Base",  description="Select the actor you would like to add a dependency to."),
+            Property.Actor(label="ActorDependency", description="Select the actor that the base actor will depend upon."),
             Property.Sensor(label="SensorDependency", description="Select the sensor that the base actor will depend upon."),
             Property.Number(label="SensorValue", description="Input the sensor value needed to switch the 'Base Actor' ON."),
             Property.Select(label="notification", options=["Yes", "No"], description="Will show notifications in case of errors if set to yes")])
@@ -22,6 +23,7 @@ class DependentActorSensor(CBPiActor):
     def on_start(self):
         self.state = False
         self.base = self.props.get("Base", None)
+        self.actor_dependency = self.props.get("ActorDependency", None)
         self.sensor_dependency = self.props.get("SensorDependency", None)
         self.sensor_min = int(self.props.get("SensorValue", 0))
         self.notification = self.props.get("notification", "Yes")
@@ -29,21 +31,19 @@ class DependentActorSensor(CBPiActor):
         pass
 
     async def on(self, power=0):
+        base_actor = self.cbpi.actor.find_by_id(self.base)
+        actor_dep = self.cbpi.actor.find_by_id(self.ActorDependency)
         sensor_dep = self.cbpi.sensor.find_by_id(self.sensor_dependency)
         sensor_val = self.cbpi.sensor.get_sensor_value(self.sensor_dependency).get("value")
-        base_actor = self.cbpi.actor.find_by_id(self.base)
         
-        if sensor_val >= self.sensor_min:
-            try:
-                await base_actor.instance.on()
-                await self.push_update()
-                self.cbpi.push_update("cbpi/actor/"+self.base, base_actor.to_dict(), True)
-                #self.state = True
-            except Exception as e:
-                self.cbpi.notify("Failed to turn on Actor", "Something is wrong with the call to turn on.",NotificationType.ERROR)
-
-#             if self.notification == "Yes":
-#                 self.cbpi.notify("Powering of Actor tbd", "This pass %s" %(sensor_dep.name) ,NotificationType.INFO)
+        try:
+            actor_dep_state = actor_dep.instance.state
+        except:
+            actor_dep_state = False
+        
+        if (actor_dep_state == False) and (sensor_val >= self.sensor_min):
+            await self.cbpi.actor.on(self.base_actor)
+            self.state = True
         else:
             await self.cbpi.actor.off(self.base)
             self.state = False
